6aacb7adcda72f1a9737a6f8bea5c53931d6306e
王迁 <wangqian@vm.ia.ac.cn>
2016-11-21 18:20:34 +0800

    modify code style

--- Project02-TipCalculator/Project02-TipCalculator/MainViewController.swift
+++ Project02-TipCalculator/Project02-TipCalculator/MainViewController.swift
@@ -13,12 +13,58 @@ class MainViewController: UIViewController {
     
     private var topHeight: CGFloat!
     
-    private let costTextField = UITextField()
+    private lazy var costTextField: UITextField = {
+        let costTF = UITextField()
+        costTF.placeholder = "$0.00"
+        costTF.font = UIFont.systemFont(ofSize: 60)
+        costTF.textAlignment = .right
+        costTF.keyboardType = .numberPad
+        costTF.addTarget(self, action: #selector(MainViewController.costTextFieldDidChange(textField:)), for: .editingChanged)
+        return costTF
+    }()
     
-    private let tipLabel = UILabel()
-    private let tipValueLabel = UILabel()
+    private lazy var tipLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .right
+        label.font = SomeConstants.font
+        label.textColor = SomeConstants.fontColor
+        return label
+    }()
     
-    private let totalValueLabel = UILabel()
+    private lazy var tipValueLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .right
+        label.font = SomeConstants.font
+        label.textColor = SomeConstants.fontColor
+        return label
+    }()
+    
+    private lazy var totalValueLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .right
+        label.font = SomeConstants.font
+        label.textColor = SomeConstants.fontColor
+        return label
+    }()
+    
+    private lazy var totalTextLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .right
+        label.text = "Total:"
+        label.font = SomeConstants.font
+        label.textColor = SomeConstants.fontColor
+        return label
+    }()
+    
+    
+    private lazy var slider: UISlider = {
+        let slider = UISlider()
+        slider.maximumValue = 100
+        slider.minimumValue = 0
+        slider.value = 15
+        slider.addTarget(self, action: #selector(self.tipPickerChanged(picker:)), for: .valueChanged)
+        return slider
+    }()
     
     private var _cost = 0.0
     private var cost: Double {
@@ -43,50 +89,43 @@ class MainViewController: UIViewController {
     }
     
     private func calculateTotal() {
-        
-        tipLabel.text = "Tip(\(tipRate)%):"
         let tipValue = cost * Double(tipRate) / 100.0
+        tipLabel.text = "Tip(\(tipRate)%):"
         tipValueLabel.text = "$\(tipValue)"
         totalValueLabel.text = "$\(tipValue + cost)"
     }
     
     private struct SomeConstants {
-        static let titleBackgroundColor: UIColor = UIColor.lightGray
-        static let textViewFont: UIFont = UIFont.systemFont(ofSize: 60)
-        static let title: String = "Tip Calculator"
         static let font: UIFont = UIFont.systemFont(ofSize: 20)
         static let fontColor: UIColor = UIColor.purple
         private init() {}
     }
+  
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func loadView() {
+        view = UIView(frame: UIScreen.main.bounds)
         
         topHeight = navigationController!.navigationBar.frame.height + UIApplication.shared.statusBarFrame.height
-        navigationItem.title = SomeConstants.title
+        navigationItem.title = "Tip Calculator"
         initView()
         
         tipRate = 15
     }
     
     private func initView() {
-        addCostTextField()
-        addTipLabel()
-        addTotalLabel()
-        addTipPicker()
+        view.addSubview(costTextField)
+        view.addSubview(tipLabel)
+        view.addSubview(tipValueLabel)
+        view.addSubview(totalTextLabel)
+        view.addSubview(totalValueLabel)
+        view.addSubview(slider)
+        
+        view.setNeedsUpdateConstraints()
     }
     
     
-    private func addCostTextField() {
-        
-        costTextField.placeholder = "$0.00"
-        costTextField.font = SomeConstants.textViewFont
-        costTextField.textAlignment = .right
-        costTextField.keyboardType = .numberPad
-        costTextField.addTarget(self, action: #selector(MainViewController.costTextFieldDidChange(textField:)), for: .editingChanged)
-        
-        view.addSubview(costTextField)
-        
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
         costTextField.snp.makeConstraints {
             maker in
             maker.top.equalTo(view.snp.top).offset(topHeight)
@@ -94,48 +133,19 @@ class MainViewController: UIViewController {
             maker.right.equalTo(view.snp.right)
             maker.height.equalTo((view.bounds.height-topHeight)/4)
         }
-    }
-    
-    private func addTipLabel() {
-        
-        tipLabel.textAlignment = .right
-        
-        view.addSubview(tipLabel)
-        tipLabel.font = SomeConstants.font
-        tipLabel.textColor = SomeConstants.fontColor
         tipLabel.snp.makeConstraints {
             maker in
-            
             maker.left.equalTo(costTextField.snp.left)
             maker.right.equalTo(view.snp.right).offset(-view.bounds.width/4)
             maker.top.equalTo(costTextField.snp.bottom)
             maker.height.equalTo((view.bounds.height-topHeight)/8)
         }
-        
-        
-        tipValueLabel.textAlignment = .right
-        
-        view.addSubview(tipValueLabel)
-        tipValueLabel.font = SomeConstants.font
-        tipValueLabel.textColor = SomeConstants.fontColor
-        
         tipValueLabel.snp.makeConstraints {
             maker in
             maker.left.equalTo(tipLabel.snp.right)
             maker.top.equalTo(tipLabel.snp.top)
             maker.size.equalTo(tipLabel.snp.size)
         }
-    }
-    
-    private func addTotalLabel() {
-        let totalTextLabel = UILabel()
-        totalTextLabel.textAlignment = .right
-        totalTextLabel.text = "Total:"
-        totalTextLabel.font = SomeConstants.font
-        totalTextLabel.textColor = SomeConstants.fontColor
-        
-        view.addSubview(totalTextLabel)
-        
         totalTextLabel.snp.makeConstraints {
             maker in
             maker.left.equalTo(tipLabel.snp.left)
@@ -143,29 +153,13 @@ class MainViewController: UIViewController {
             maker.size.equalTo(tipLabel.snp.size)
         }
         
-        totalValueLabel.textAlignment = .right
-        totalValueLabel.font = SomeConstants.font
-        totalValueLabel.textColor = SomeConstants.fontColor
-        view.addSubview(totalValueLabel)
-        
         totalValueLabel.snp.makeConstraints {
             maker in
             maker.top.equalTo(tipLabel.snp.bottom)
             maker.left.equalTo(tipLabel.snp.right)
             maker.size.equalTo(tipLabel.snp.size)
         }
-        
-    }
-    
-    private func addTipPicker() {
-        let slider = UISlider()
-        slider.maximumValue = 100
-        slider.minimumValue = 0
-        slider.value = 15
-        slider.addTarget(self, action: #selector(self.tipPickerChanged(picker:)), for: .valueChanged)
-        
-        view.addSubview(slider)
-        
+
         slider.snp.makeConstraints {
             maker in
             maker.top.equalTo(totalValueLabel.snp.bottom)
@@ -174,6 +168,7 @@ class MainViewController: UIViewController {
             maker.height.equalTo(20)
         }
     }
+
     
     @objc private func tipPickerChanged(picker: UISlider) {
         costTextField.resignFirstResponder()
